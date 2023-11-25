db23812a27c349d7932b99aee803b025e800ce0d
Nicolas Cianni <nicocianni21@gmail.com>
2023-11-25 10:20:01 -0300

    prueba

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <nav className="NavBar" >
             <Link to="/">
-            <h3>Tienda Online</h3>
+            <h3>Tienda Onlinee</h3>
             </Link>
             <div className="Categories">
                 <NavLink to={`/category/graficas`} className={({ isActive}) => isActive ? "ActiveOption" : Option}>Graficas</NavLink>
